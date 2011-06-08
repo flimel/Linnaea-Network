c61250738dbc61a885783c3dd53c9ef5acbbd26e
Melissa Flint <mtflint@gmail.com>
2011-06-07 20:42:30 -0700

    Add username to database

--- create.php
+++ create.php
@@ -4,8 +4,8 @@ if ('POST' === $_SERVER['REQUEST_METHOD'])
 {
   $conn = new PDO('mysql:dbname=linnaeanetwork', 'root');
 
-  $stmt = $conn->prepare('INSERT INTO info (name, year) VALUES (?, ?)');
-  $stmt->execute(array($_POST['name'], $_POST['year']));
+  $stmt = $conn->prepare('INSERT INTO info (name, username, year) VALUES (?, ?, ?)');
+  $stmt->execute(array($_POST['name'], $_POST['username'], $_POST['year']));
 
   header('Location: thankyou.html');
 }
